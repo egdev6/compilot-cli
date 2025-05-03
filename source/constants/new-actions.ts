51e418174f989e339e73cdebf651d34f656be44c
Quique <enrique.gomez@scrm.lidl>
2025-05-03 15:19:03 +0200

    Apply code formatting changes

--- source/constants/new-actions.ts
+++ source/constants/new-actions.ts
@@ -1,6 +1,6 @@
 const newActions = [
-	{ label: "🔁 Create another", value: "again" },
-	{ label: "🚪 Exit", value: "exit" },
+  { label: '🔁 Create another', value: 'again' },
+  { label: '🚪 Exit', value: 'exit' }
 ];
 
 export default newActions;
--- source/plop-files/types/component.ts
+++ source/plop-files/types/component.ts
@@ -1,120 +1,103 @@
-import path from "node:path";
-import type { DataProps } from "@/models/app.js";
-import { getFolderByConfig } from "@utils/functions.js";
+import path from 'node:path';
+import type { DataProps } from '@/models/app.js';
+import { getFolderByConfig } from '@utils/functions.js';
 
-export const addComponentActions = (
-	actions: any,
-	data: DataProps,
-	srcRoot: string,
-	config: any,
-) => {
-	if (data?.type === "component") {
-		const folderName = getFolderByConfig(
-			config.components.naming.folder,
-			data.name || "",
-		);
-		const basePath = path.join(
-			srcRoot,
-			`${config.components.base}/{{folder}}/${folderName}/`,
-		);
-		const language = config.config.language;
-		const templatesPath = path.join(
-			srcRoot,
-			`source/plop-files/templates/${language}/component`,
-		);
+export const addComponentActions = (actions: any, data: DataProps, srcRoot: string, config: any) => {
+  if (data?.type === 'component') {
+    const folderName = getFolderByConfig(config.components.naming.folder, data.name || '');
+    const basePath = path.join(srcRoot, `${config.components.base}/{{folder}}/${folderName}/`);
+    const language = config.config.language;
+    const templatesPath = path.join(srcRoot, `source/plop-files/templates/${language}/component`);
 
-		data.config = config.components;
+    data.config = config.components;
 
-		if (data?.folder) {
-			const folderSegments = data.folder.split("/");
-			const groupIndex = folderSegments.indexOf("components") + 1;
-			const groupName = config.components.atomic
-				? folderSegments[groupIndex]
-				: undefined;
+    if (data?.folder) {
+      const folderSegments = data.folder.split('/');
+      const groupIndex = folderSegments.indexOf('components') + 1;
+      const groupName = config.components.atomic ? folderSegments[groupIndex] : undefined;
 
-			if (groupName) {
-				data.storyGroup =
-					groupName.charAt(0).toUpperCase() + groupName.slice(1);
-			}
-		}
+      if (groupName) {
+        data.storyGroup = groupName.charAt(0).toUpperCase() + groupName.slice(1);
+      }
+    }
 
-		switch (language) {
-			case "typescript":
-				if (config.components.files.types === "file") {
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `types.ts`),
-						templateFile: `${templatesPath}/types.ts.hbs`,
-					});
-				}
-				if (config.components.files.stories) {
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `{{pascalName}}.stories.tsx`),
-						templateFile: `${templatesPath}/stories.tsx.hbs`,
-					});
-				}
-				if (config.components.files.test) {
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `{{pascalName}}.test.tsx`),
-						templateFile: `${templatesPath}/test.tsx.hbs`,
-					});
-				}
-				if (config.components.files.types === "inline") {
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `{{pascalName}}.tsx`),
-						templateFile: `${templatesPath}/component-inline.tsx.hbs`,
-					});
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `index.ts`),
-						templateFile: `${templatesPath}/index-inline.ts.hbs`,
-					});
-				} else {
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `{{pascalName}}.tsx`),
-						templateFile: `${templatesPath}/component-file.tsx.hbs`,
-					});
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `index.ts`),
-						templateFile: `${templatesPath}/index-file.ts.hbs`,
-					});
-				}
-				break;
-			case "javascript":
-				if (config.components.files.stories) {
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `{{pascalName}}.stories.jsx`),
-						templateFile: `${templatesPath}/stories.jsx.hbs`,
-					});
-				}
-				if (config.components.files.test) {
-					actions.push({
-						type: "add",
-						path: path.join(basePath, `{{pascalName}}.test.jsx`),
-						templateFile: `${templatesPath}/test.jsx.hbs`,
-					});
-				}
-				actions.push({
-					type: "add",
-					path: path.join(basePath, `{{pascalName}}.jsx`),
-					templateFile: `${templatesPath}/component.jsx.hbs`,
-				});
-				actions.push({
-					type: "add",
-					path: path.join(basePath, `index.js`),
-					templateFile: `${templatesPath}/index.js.hbs`,
-				});
-				break;
-			default:
-				break;
-		}
-	}
+    switch (language) {
+      case 'typescript':
+        if (config.components.files.types === 'file') {
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `types.ts`),
+            templateFile: `${templatesPath}/types.ts.hbs`
+          });
+        }
+        if (config.components.files.stories) {
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `{{pascalName}}.stories.tsx`),
+            templateFile: `${templatesPath}/stories.tsx.hbs`
+          });
+        }
+        if (config.components.files.test) {
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `{{pascalName}}.test.tsx`),
+            templateFile: `${templatesPath}/test.tsx.hbs`
+          });
+        }
+        if (config.components.files.types === 'inline') {
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `{{pascalName}}.tsx`),
+            templateFile: `${templatesPath}/component-inline.tsx.hbs`
+          });
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `index.ts`),
+            templateFile: `${templatesPath}/index-inline.ts.hbs`
+          });
+        } else {
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `{{pascalName}}.tsx`),
+            templateFile: `${templatesPath}/component-file.tsx.hbs`
+          });
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `index.ts`),
+            templateFile: `${templatesPath}/index-file.ts.hbs`
+          });
+        }
+        break;
+      case 'javascript':
+        if (config.components.files.stories) {
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `{{pascalName}}.stories.jsx`),
+            templateFile: `${templatesPath}/stories.jsx.hbs`
+          });
+        }
+        if (config.components.files.test) {
+          actions.push({
+            type: 'add',
+            path: path.join(basePath, `{{pascalName}}.test.jsx`),
+            templateFile: `${templatesPath}/test.jsx.hbs`
+          });
+        }
+        actions.push({
+          type: 'add',
+          path: path.join(basePath, `{{pascalName}}.jsx`),
+          templateFile: `${templatesPath}/component.jsx.hbs`
+        });
+        actions.push({
+          type: 'add',
+          path: path.join(basePath, `index.js`),
+          templateFile: `${templatesPath}/index.js.hbs`
+        });
+        break;
+      default:
+        break;
+    }
+  }
 
-	return actions;
+  return actions;
 };
